30a1ec83155acbfca894f484e0eb61b65a6efb6e
shubham.sc <shubham@skills.cafe>
2025-03-10 16:25:16 +0530

    read data completed

--- index.php
+++ index.php
@@ -5,72 +5,57 @@ include 'header.php';
     <h2>All Records</h2>
     <table cellpadding="7px">
         <thead>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Address</th>
-        <th>Class</th>
-        <th>Phone</th>
-        <th>Action</th>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Class</th>
+            <th>Phone</th>
+            <th>Action</th>
         </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>Ramesh</td>
-                <td>Delhi</td>
-                <td>BCA</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Suresh</td>
-                <td>Punjab</td>
-                <td>BCOM</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>Suresh</td>
-                <td>Haryana</td>
-                <td>BSC</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>4</td>
-                <td>Krishna</td>
-                <td>Gujrat</td>
-                <td>BCA</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>5</td>
-                <td>Rohit</td>
-                <td>Delhi</td>
-                <td>BCA</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-        </tbody>
+
+        <?php
+        include 'config.php';
+
+        $sql = "SELECT * FROM student JOIN CLASS ON student.sclass = class.cid";
+
+        $response = mysqli_query($conn, $sql) or die("sql query failed");
+
+        if (mysqli_num_rows($response) > 0) {
+        ?>
+
+            <tbody>
+
+                <?php
+                while ($row = mysqli_fetch_assoc($response)) {
+                ?>
+
+                    <tr>
+                        <td><?php echo $row['sid'];  ?></td>
+                        <td><?php echo $row['sname'];  ?></td>
+                        <td><?php echo $row['saddress'];  ?></td>
+                        <td><?php echo $row['classname'];  ?></td>
+                        <td><?php echo $row['sphone'];  ?></td>
+                        <td>
+                            <a href='edit.php'>Edit</a>
+                            <a href='delete-inline.php'>Delete</a>
+                        </td>
+                    </tr>
+
+                <?php
+                }
+                ?>
+            </tbody>
+
+        <?php
+        } else {
+            echo "<h2>No data</h2>";
+        }
+
+        mysqli_close($conn);
+        ?>
     </table>
 </div>
 </div>
 </body>
+
 </html>
